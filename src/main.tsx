5462b8f17b5f866787ea63ffec914b8548fd563e
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2024-07-06 19:56:13 +0530

    Added dashboard

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./styles/app.scss";
+import App from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// react-icons/fa (imported by E:/dashboard/Project Setup/src/components/admin/AdminSidebar.tsx)
+// react-icons/bi (imported by E:/dashboard/Project Setup/src/pages/admin/dashboard.tsx)
+// react-icons/bs (imported by E:/dashboard/Project Setup/src/pages/admin/dashboard.tsx)
+// react-icons/ai (imported by E:/dashboard/Project Setup/src/components/admin/TableHOC.tsx)
+// react-icons/hi (imported by E:/dashboard/Project Setup/src/components/admin/AdminSidebar.tsx)
+// react-icons/io (imported by E:/dashboard/Project Setup/src/components/admin/AdminSidebar.tsx)
+// react-icons/ri (imported by E:/dashboard/Project Setup/src/components/admin/AdminSidebar.tsx)
+// chart.js (imported by E:/dashboard/Project Setup/src/components/admin/Charts.tsx)
+// react-chartjs-2 (imported by E:/dashboard/Project Setup/src/components/admin/Charts.tsx)
+// react-table (imported by E:/dashboard/Project Setup/src/components/admin/TableHOC.tsx)
